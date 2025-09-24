db0cf47f4a787f759612104da1e9b10fa62c03bf
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2025-09-24 21:06:02 +0900

    Create phi_publisher_bh1750.ino

---
+++ phi-board/phi_publisher_bh1750.ino
@@ -0,0 +1,40 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+#include <Wire.h>
+#include <BH1750.h>
+BH1750 lightMeter;
+
+const char* ssid = "RiatechChief_2G";
+const char* password = "12345678";
+
+const char* userId = "mqtt_phirippa";
+const char* userPw = "1234";
+const char* clientId = userId;
+char *topic = "Sensors/MyOffice/Indoor/Lux";
+char* server = "192.168.1.12"; 
+WiFiClient wifiClient; 
+PubSubClient client(server, 1883, wifiClient);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.print("\nConnecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println("\nWiFi Connected");
+  Serial.print("Local IP address : ");
+  Serial.println(WiFi.localIP());
+
+  Serial.println("Connecting to broker");
+  while ( !client.connect(clientId, userId, userPw) ){ 
+    Serial.print("*");
+    delay(1000);
+  }
+  Serial.println("\nConnected to broker");
+  Wire.begin();
+  lightMeter.begin();
+}
