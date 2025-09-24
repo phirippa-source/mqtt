fa616fc510dacb3b1b9168179dbaf7ac2faa5269
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2025-09-24 21:08:01 +0900

    Create phi_publish_dht11.ino

---
+++ phi-board/phi_publish_dht11.ino
@@ -0,0 +1,65 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// ----- DHT11 ------------------------
+#include "DHT.h"
+#define DHTPIN D4     
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+// -------------------------------------
+
+const char* ssid = "RiatechChief_2G";
+const char* password = "12345678";
+
+const char* userId = "phirippa";
+const char* userPw = "1234";
+const char* clientId = userId;
+char *topic_t = “Sensors/MyOffice/Indoor/Temp";
+char *topic_h = “Sensors/MyOffice/Indoor/Humi";
+char* server = "192.168.1.12"; 
+
+WiFiClient wifiClient; 
+PubSubClient client(server, 1883, wifiClient);
+
+void setup() {
+  Serial.begin(9600);
+ 
+  Serial.print("\nConnecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("\nWiFi Connected");
+  
+  Serial.println("Connecting to broker");
+  while ( !client.connect(clientId, userId, userPw) ){ 
+    Serial.print("*");
+    delay(1000);
+  }
+  Serial.println("\nConnected to broker");
+  dht.begin();
+}
+
+void loop() {
+  char buf[20];
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  if (isnan(h) || isnan(t) ) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  String str = String(h);
+  str.toCharArray(buf, str.length());
+  client.publish(topic_h, buf);
+  Serial.println(String(topic_h) + " : " + buf);
+  
+  str = String(t);
+  str.toCharArray(buf, str.length());
+  client.publish(topic_t, buf);
+  Serial.println(String(topic_t)  + " : " + buf);
+  delay(2000);
+}
