432225c8847d92a7190318d3285593cb0da85f29
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2025-09-24 21:10:21 +0900

    Create phi_subscriber.ino

---
+++ phi-board/phi_subscriber.ino
@@ -0,0 +1,46 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "RiatechChief_2G";
+const char* password = "12345678";
+
+const char* clientId = "phirippa";
+const char* userId = clientId;
+const char* userPw = "1234";
+char *topic = “Sensors/MyOffice/Indoor/#";
+char* server = "192.168.1.12"; 
+char messageBuf[100];
+
+void callback(char* topic, byte* payload, unsigned int length) {  
+  Serial.println("Message arrived!\nTtopic: " + String(topic));
+  Serial.println("Length: "+ String(length, DEC));
+  
+  strncpy(messageBuf, (char*)payload, length);
+  messageBuf[length] = '\0';
+  Serial.println("Payload: "+ String(messageBuf) + "\n\n");
+}
+
+WiFiClient wifiClient; 
+PubSubClient client(server, 1883, callback, wifiClient);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.print("\nConnecting to ");   Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");   delay(500);
+  }
+  Serial.println("\nWiFi Connected");
+  
+  while ( !client.connect(clientId, userId, userPw) ){ 
+    Serial.print("*");    delay(1000);
+  }
+  Serial.println("\nConnected to broker");
+  Serial.println(String("Subscribing! topic = ") + topic);
+  client.subscribe(topic);
+ }
+
+void loop() {  
+  client.loop();
+}
