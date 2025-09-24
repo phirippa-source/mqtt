282b4536c00a93404ba96dc79b779ce67f0a74fe
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2025-09-24 21:13:07 +0900

    Create phi_sub_pub.ino

---
+++ phi-board/phi_sub_pub.ino
@@ -0,0 +1,76 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+#include <BH1750.h>
+
+BH1750 lightMeter;
+#define USER_LED  D0
+
+const char* ssid = "RiatechChief_2G";
+const char* password = "12345678";
+
+const char* userId = “mqtt_phirippa";
+const char* userPw = "1234";
+const char* clientId = userId;
+
+char *topicSub = “Sensors/MyOffice/Indoor/lamp";
+char *topicPub = “Sensors/MyOffice/Indoor/Lux";
+
+char* server = "192.168.1.12"; 
+char messageBuf[100];
+
+void callback(char* topic, byte* payload, unsigned int length) {  
+  Serial.println("Message arrived!\nTtopic: " + String(topic));
+  Serial.println("Length: "+ String(length, DEC));
+  
+  strncpy(messageBuf, (char*)payload, length);
+  messageBuf[length] = '\0';
+  String ledState = String(messageBuf);
+  Serial.println("Payload: "+ ledState + "\n\n");
+
+  if( ledState == “1"  ){      
+    digitalWrite(USER_LED, LOW);
+  } else if (ledState==“0") { 
+    digitalWrite(USER_LED, HIGH);
+  } else { 
+    Serial.println("Wrong Message");
+  }
+}
+
+WiFiClient wifiClient; 
+PubSubClient client(server, 1883, callback, wifiClient);
+
+void setup() {
+  pinMode(USER_LED, OUTPUT);
+  Serial.begin(9600);
+  Wire.begin();
+  lightMeter.begin();
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("\nWiFi Connected");
+ 
+  while ( !client.connect(clientId, userId, userPw) ){ 
+    Serial.print("*");
+    delay(1000);
+  }
+  Serial.println("\nConnected to broker");
+  Serial.print("Subscribing! topic = ");
+  Serial.println(topicSub);
+  client.subscribe(topicSub);
+ }
+
+void loop() {
+  client.loop();
+
+  char buf[20];
+  String strLuxValue =  String( lightMeter.readLightLevel() );
+  strLuxValue.toCharArray(buf, strLuxValue.length() );
+  client.publish(topicPub, buf);
+  Serial.println(String(topicPub) + " : " + buf);
+
+  delay(7000);
+}
