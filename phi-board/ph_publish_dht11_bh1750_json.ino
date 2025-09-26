7cd9a5b120348b7f4d2c9f0f434747b7c8ca6c14
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2025-09-26 14:16:20 +0900

    Update ph_publish_dht11_bh1750_json.ino

--- phi-board/ph_publish_dht11_bh1750_json.ino
+++ phi-board/ph_publish_dht11_bh1750_json.ino
@@ -4,8 +4,8 @@
 #include <Wire.h>
 #include <BH1750.h>
 
-#define DHTPIN 26     
-#define DHTTYPE DHT11  
+#define DHTPIN D4
+#define DHTTYPE DHT11
 
 DHT dht(DHTPIN, DHTTYPE);
 BH1750 lightMeter;
@@ -19,40 +19,38 @@ const char *topic = "MyOffice/Indoor/Value";
 const int PAYLOADSIZE = 128;
 
 void setup() {
-   dht.begin();
-   Wire.begin();
-   lightMeter.begin();
-   Serial.begin(115200);
-   Serial.print("\nConnecting to "); Serial.println(ssid);
-   WiFi.begin(ssid, password);
-   while (WiFi.status() != WL_CONNECTED) {
-      Serial.print("."); delay(500);
-   }
-   Serial.println("\nWiFi Connected");
-   
-   Serial.print("Connecting to broker(" + String(serverIPAddr) + ")");
-   while ( !client.connect(clientId, userId, userPw) ){ 
-      Serial.print("*"); delay(500);
-   }
-   Serial.println("\nConnected to broker!!!");
+  dht.begin();
+  Wire.begin();
+  lightMeter.begin();
+  Serial.begin(9600);
+  Serial.print("\nConnecting to "); Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print("."); delay(500);
+  }
+  Serial.println("\nWiFi Connected");
+
+  Serial.print("Connecting to broker(" + String(serverIPAddr) + ")");
+  while ( !client.connect(clientId, userId, userPw) ){ 
+    Serial.print("*"); delay(500);
+  }
+  Serial.println("\nConnected to broker!!!");
 }
 
 void loop() {
-   char payload[PAYLOADSIZE];
-   float h = dht.readHumidity();   float t = dht.readTemperature();
-   if (isnan(h) || isnan(t) ) {
-      Serial.println("Failed to read from DHT sensor!");  return;
-   }
-   float lux = lightMeter.readLightLevel();
-   String sPayload = "{\"Temp\":"
-                  + String(t, 1)
-                  + ",\"Humi\":"
-                  + String(h, 1)
-                  +",\"Lux\":"
-                  + String(lux, 1)
-                  + "}";
-   sPayload.toCharArray(payload, PAYLOADSIZE);
-   client.publish(topic, payload);
-   Serial.print(String(topic) + " ");  Serial.println(payload);            
-   delay(1000);
+  char payload[PAYLOADSIZE];
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  if (isnan(h) || isnan(t) ) {
+    Serial.println("Failed to read from DHT sensor!");  return;
+  }
+  float lux = lightMeter.readLightLevel();
+  String sPayload = "{\"Temp\":" + String(t, 1)
+                  + ",\"Humi\":" + String(h, 1)
+                  +",\"Lux\":"   + String(lux, 1) + "}";
+  sPayload.toCharArray(payload, PAYLOADSIZE);
+  client.publish(topic, payload);
+  Serial.print(String(topic) + " "); 
+  Serial.println(payload);
+  delay(1000);
 }
